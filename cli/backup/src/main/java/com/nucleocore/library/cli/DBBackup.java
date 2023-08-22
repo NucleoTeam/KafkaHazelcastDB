a1cb1c88009b3b541f1c9f9b21246a735ecaed23
Nathaniel <nathaniel.davidson@gmail.com>
2023-08-22 01:28:53 -0600

    build maven, and build docker

--- cli/backup/src/main/java/com/nucleocore/library/cli/DBBackup.java
+++ cli/backup/src/main/java/com/nucleocore/library/cli/DBBackup.java
@@ -7,7 +7,16 @@ import java.nio.file.Paths;
 
 public class DBBackup{
   public static void main(String[] args) throws ClassNotFoundException {
+    String sourceKafkaHosts = System.getenv().getOrDefault("SOURCE_KAFKA_HOSTS","127.0.0.1:29092");
+    String sourceTable = System.getenv().getOrDefault("SOURCE_TABLE","temptable1");
+    String clazz = System.getenv().getOrDefault("TABLE_CLASS","");
+    String targetTable = System.getenv().getOrDefault("TARGET_TABLE","restored");
+
     NucleoDB db = new NucleoDB();
-    db.launchReadOnlyTable(args[0], args[1], Class.forName(args[3])).setTableFileName(Paths.get(args[2],  args[1]+".dat").toString()).setSaveChanges(true).build();
+    db
+        .launchReadOnlyTable(sourceKafkaHosts, sourceTable, Class.forName(clazz))
+        .setTableFileName(Paths.get("./data/",  targetTable+".dat").toString())
+        .setSaveChanges(true)
+        .build();
   }
 }
--- cli/restore/src/main/java/com/nucleocore/library/cli/DBRestore.java
+++ cli/restore/src/main/java/com/nucleocore/library/cli/DBRestore.java
@@ -5,16 +5,23 @@ import com.nucleocore.library.database.utils.Serializer;
 
 public class DBRestore{
   public static void main(String[] args) throws ClassNotFoundException {
+
+    String sourceKafkaHosts = System.getenv().getOrDefault("SOURCE_KAFKA_HOSTS","127.0.0.1:29092");
+    String sourceTable = System.getenv().getOrDefault("SOURCE_TABLE","temptable1");
+    String clazz = System.getenv().getOrDefault("TABLE_CLASS","");
+    String targetKafkaHosts = System.getenv().getOrDefault("TARGET_KAFKA_HOSTS","127.0.0.1:29092");
+    String targetTable = System.getenv().getOrDefault("TARGET_TABLE","restored");
+
     NucleoDB db = new NucleoDB();
     Serializer.log(args);
-    db.launchLocalOnlyTable(args[0], "temptable1", Class.forName(args[3]), table->{
+    db.launchLocalOnlyTable(sourceKafkaHosts, "temptable1", Class.forName(clazz), table->{
       try {
-        table.exportTo(db.launchWriteOnlyTable(args[0], args[2], Class.forName(args[3])).build());
+        table.exportTo(db.launchWriteOnlyTable(targetKafkaHosts, targetTable, Class.forName(clazz)).build());
         System.exit(0);
       } catch (ClassNotFoundException e) {
         throw new RuntimeException(e);
       }
-    }).setTableFileName("./data/" + args[1] + ".dat").build();
+    }).setTableFileName("./data/" + sourceTable + ".dat").build();
 
   }
 }

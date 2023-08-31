1e1cff6fdf120498539426dbe0a1331a185bdc4e
Nathaniel <nathaniel.davidson@gmail.com>
2023-08-31 01:18:56 -0600

    publish new gha

--- cli/restore/src/main/java/com/nucleocore/library/cli/DBRestore.java
+++ cli/restore/src/main/java/com/nucleocore/library/cli/DBRestore.java
@@ -8,7 +8,9 @@ public class DBRestore{
 
     String sourceKafkaHosts = System.getenv().getOrDefault("SOURCE_KAFKA_HOSTS","127.0.0.1:29092");
     String sourceTable = System.getenv().getOrDefault("SOURCE_TABLE","temptable1");
+
     String clazz = System.getenv().getOrDefault("TABLE_CLASS","");
+
     String targetKafkaHosts = System.getenv().getOrDefault("TARGET_KAFKA_HOSTS","127.0.0.1:29092");
     String targetTable = System.getenv().getOrDefault("TARGET_TABLE","restored");
 

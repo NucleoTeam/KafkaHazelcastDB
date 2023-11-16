083d432bdf8ca71328b7c991b0dce593eae44985
Nathaniel <nathaniel.davidson@gmail.com>
2023-11-15 21:08:10 -0700

    disable autocommit

--- library/src/main/java/com/nucleocore/library/kafkaLedger/ConsumerHandler.java
+++ library/src/main/java/com/nucleocore/library/kafkaLedger/ConsumerHandler.java
@@ -180,8 +180,8 @@ public class ConsumerHandler implements Runnable{
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
     //System.out.println(groupName);
     props.put(ConsumerConfig.GROUP_ID_CONFIG, groupName);
-    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    //props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
